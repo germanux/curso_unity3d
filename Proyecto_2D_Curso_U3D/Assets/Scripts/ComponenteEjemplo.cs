9ab192895015092d9ed922584a7bc72b926a853c
German <germanux@gmail.com>
2018-11-12 19:46:48 +0100

    Primeros ejemplos botones

---
+++ Proyecto_2D_Curso_U3D/Assets/Scripts/ComponenteEjemplo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponenteEjemplo : MonoBehaviour {
+
+    public float posicionX = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hola mundo!"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(posicionX, 0, 0);
+	}
+}
