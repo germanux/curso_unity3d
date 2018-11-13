55b2b8612917589854863f4bc0448ba951b54bfa
German <germanux@gmail.com>
2018-11-13 17:43:29 +0100

    Materiales Instanciando prefabs de cilindros de colores

---
+++ Proyecto_2D_Curso_U3D/Assets/Scripts/InstanciarPrefab.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanciarPrefab : MonoBehaviour {
+
+    public GameObject cilindro;
+    public Vector3 posicion;
+
+    public void InstanciarCilindro()
+    {
+        GameObject cilindroCreado;
+        cilindroCreado = Instantiate<GameObject>(cilindro);
+        cilindroCreado.transform.position = posicion;
+    }
+}
