50fa564da52fc81e916253662a078c6c6ee7b3a1
German <germanux@gmail.com>
2018-11-13 20:22:35 +0100

    Material con textura propia. Sonidos y música

---
+++ Proyecto_2D_Curso_U3D/Assets/Scripts/ReproductorSonido.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproductorSonido : MonoBehaviour {
+
+    public AudioSource audio;
+    public float volumen;
+
+	// Use this for initialization
+	public void Play()
+    {
+        audio.volume = volumen;
+        audio.Play(500);
+    }
+}
